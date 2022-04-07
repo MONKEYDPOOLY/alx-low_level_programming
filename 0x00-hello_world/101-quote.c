b69adbb308fd7e4c05d532964c1fd7ebfa46c729
MONKEYDPOOLY <dppooly27@gmail.com>
2022-04-07 22:25:26 +0000

    My quote C UNIX commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-write(1, "and that place of art is useful\" - Dora Korpar, 2015-10-19,\n");
+write(1, "and that place of art is useful\" - Dora Korpar, 2015-10-19\n", 1);
 return (1);
 }

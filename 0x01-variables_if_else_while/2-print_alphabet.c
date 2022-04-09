99169fadd0d44281b5e3d83521c01bd9129a90cb
MONKEYDPOOLY <dppooly27@gmail.com>
2022-04-09 13:15:56 +0000

    My i sometimes suffer from insomnia commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/success)
+ */
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= '2'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}

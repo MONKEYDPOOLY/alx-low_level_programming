b8ba0ede954a2e4fe3b3bc2b955aa6f5a94f2684
MONKEYDPOOLY <dppooly27@gmail.com>
2022-04-09 16:32:42 +0000

    My reverse alphabet commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,11 +5,11 @@
  * Return: Always 0 (Success/correct)
  */
 
-int main (void)
+int main(void)
 
 {
 char alphabet;
-for(alphabet = 122 ; alphabet >= 97; alphabet--)
+for (alphabet = 122 ; alphabet >= 97; alphabet--)
 {
 putchar(alphabet);
 }

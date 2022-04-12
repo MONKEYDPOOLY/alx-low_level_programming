caab74592f205983ec6b584ae89ec63efb3df744
MONKEYDPOOLY <dppooly27@gmail.com>
2022-04-12 22:41:31 +0000

    My learn your times table commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,6 @@ void times_table(void)
 int a;
 int b;
 int c;
-
 for (a = 0; a <= 9; a++)
 {
 for (b = 0; b <= 9; b++)
@@ -32,7 +31,6 @@ _putchar(',');
 _putchar(' ');
 }
 }
-
 else
 {
 _putchar ((c / 10) + '0');

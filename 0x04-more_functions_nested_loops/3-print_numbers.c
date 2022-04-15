2f10ad531f1448e3d8ff96b5a458c087d36c443b
MONKEYDPOOLY <dppooly27@gmail.com>
2022-04-15 01:21:33 +0000

    My numbers speak for themselves commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_numbers - print number zero to nine
  *
@@ -7,12 +6,14 @@
  */
 void print_numbers(void)
 {
+
 int i;
 
 i = 0;
+
 for (i = 0; i < 10; i++)
 
-_putchar (i + '0');
+_putchar(i + '0');
 
 _putchar ('\n');
 }
